6e892d62092d855667d284f375bb933c0591cd49
LeoAlberge <alberge.leo@gmail.com>
2024-02-21 23:48:05 +0100

    Update viz_preprocessed.py

--- binaries/visualization/viz_preprocessed.py
+++ binaries/visualization/viz_preprocessed.py
@@ -17,14 +17,15 @@ def main():
         seg_data = y[0, :, :, :]
         os.makedirs(outdir, exist_ok=True)
         for k in range(vol_data.shape[0]):
-            plt.figure()
-            plt.imshow(vol_data[k, :, :], cmap="gray", interpolation="bilinear")
-            for id, l in TOTAL_SEG_CLASS_ID_TO_LABELS.items():
-                if (seg_data[k, :, :] == id).sum() > 0:
-                    plt.contour((seg_data[k, :, :] == id).astype(np.uint8), alpha=0.5,
-                                levels=[0.5], colors=[colors[l]])
-            plt.savefig(os.path.join(outdir, f"{k}.png"))
-            plt.close()
+            if seg_data[k, :, :].sum() > 0:
+                plt.figure()
+                plt.imshow(vol_data[k, :, :], cmap="gray", interpolation="bilinear")
+                for id, l in TOTAL_SEG_CLASS_ID_TO_LABELS.items():
+                    if (seg_data[k, :, :] == id).sum() > 0:
+                        plt.contour((seg_data[k, :, :] == id).astype(np.uint8), alpha=0.5,
+                                    levels=[0.5], colors=[colors[l]])
+                plt.savefig(os.path.join(outdir, f"{k}.png"))
+                plt.close()
 
 
 if __name__ == '__main__':
